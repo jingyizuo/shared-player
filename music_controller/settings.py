f6f0d22791151cee6b9c354fc4ddcc5d8682f5be
JingyiZuo <jzuo1998@gmail.com>
2022-08-29 12:57:09 -0500

    Update settings.py

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8l-7inowc&@bcc+tbn_#avl2bord)+okucx+&%__-gjvyql6d8'
+#SECRET_KEY = 'django-insecure-8l-7inowc&@bcc+tbn_#avl2bord)+okucx+&%__-gjvyql6d8'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-8l-7inowc&@bcc+tbn_#avl2bord)+okucx+&%__-gjvyql6d8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
